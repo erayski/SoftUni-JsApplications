3de5514118a2293ac50037c348e93678fcf18006
Eray Seyhan <eray_seyhan@abv.bg>
2021-12-06 21:34:22 +0200

    Delete app.js

--- 03.AsynchronousProgramming-Lab/01.XHR/scripts/app.js
+++
@@ -1,8 +0,0 @@
-function loadRepos() {
-   const url = 'https://api.github.com/users/testnakov/repos';
-   const addInfo = document.getElementById("res");
-
-   fetch(url)
-      .then(res => res.json())
-      .then(data => addInfo.textContent = JSON.stringify(data))
-}
